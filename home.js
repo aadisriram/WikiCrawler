c5aa941e01a56acc716cfda113467e65cae6ddb1
Aaditya Sriram <aadisriram@gmail.com>
2015-05-04 23:32:29 -0400

    Added multiple start points

--- home.js
+++ home.js
@@ -4,18 +4,68 @@
 
 var WikiUrlGrabber = require("./index.js");
 
-var kafka = require('kafka-node'),
-    Producer = kafka.Producer,
-    client = new kafka.Client('152.46.20.89:2181/');
+var nodeDrpc = require('node-drpc');
+var nodeDrpcClient =  new  nodeDrpc("152.46.17.135", 3772);
+
+var kafka = require('kafka-node');
 
 var prod = new kafka.Producer(new kafka.Client());
 
-var prof = new WikiUrlGrabber("https://en.wikipedia.org/wiki/The_Tower_House", function(outlinks) {
-    outlinks.forEach(function(outlink) {
-        console.log(outlink);
-        payloads = [{topic : "wiki_url_queue", messages : JSON.stringify(outlink)}];
-        prod.send(payloads, function(err, data) {
-            console.log(err);
+var consumer = new kafka.Consumer(new kafka.Client(), [{ topic : "wiki_url_queue"}]);
+
+var baseURL = "https://en.wikipedia.org/wiki/";
+
+var starting_points = ["Science", "Business", "Sports",
+    "Medicine", "Literature", "Religion",
+    "Politics", "Crime", "Geography", "History"];
+
+starting_points.forEach(function(startVal) {
+    //var payloads = [{topic : "wiki_url_queue", messages : JSON.stringify(startVal), attributes : 2}];
+    //prod.send(payloads, function(err, data) {
+    //    if(err != null)
+    //        console.log(err.message);
+    //});
+
+    crawl(startVal);
+});
+
+//crawl("Racism");
+
+function crawl(url) {
+    new WikiUrlGrabber(url, function(results) {
+        console.log(results);
+
+        nodeDrpcClient.execute("check_url", baseURL + url, function(err, response) {
+           console.log(response);
+           if(response.indexOf("false") > -1) {
+               //Push the results for the current URL to kafka to be consumed by storm
+               console.log(results);
+               var payload_storm = [
+                   {topic : "crawler_feed1", messages : JSON.stringify(results), attributes : 2},
+                   {topic : "crawler_feed2", messages : JSON.stringify(results), attributes : 2},
+                   {topic : "crawler_feed3", messages : JSON.stringify(results), attributes : 2}
+               ];
+
+               prod.send(payload_storm, function(err, data) {
+                  if(err != null) {
+                      console.error("Failed to send to kafka" + err.message);
+                  }
+               });
+
+               //Pushing the URL's that need to be crawled next into a kafka queue
+               results["urls"].forEach(function(result) {
+
+                   payloads = [{topic : "wiki_url_queue", messages : JSON.stringify(result), attributes : 2}];
+                   prod.send(payloads, function(err, data) {
+                       if(err != null)
+                        console.log(err.message);
+                   });
+               });
+           }
         });
     });
+}
+
+consumer.on('message', function (message) {
+    crawl(message.value.replace(/['"]+/g, ''));
 });
--- index.js
+++ index.js
@@ -4,23 +4,44 @@
 
 var jsdom = require("jsdom");
 
-function getWikiUrls(baseUrl, callback) {
+var baseUrl = "https://en.wikipedia.org/wiki/";
+
+function getWikiUrls(link, callback) {
     var outlinks = [];
     jsdom.env({
-        url: baseUrl,
+        url: baseUrl + link,
         scripts: ["http://code.jquery.com/jquery.js"],
         done: function (errors, window) {
-            var $ = window.$;
+            if (window != undefined) {
+                var $ = window.$;
 
-            var anchors = $("#mw-content-text p").find("a").each(function() {
-                var url = $(this).attr('href');
-                if(/^(\/wiki)/.test(url)) {
-                    //console.log(url);
-                    outlinks.push(url);
-                }
-            });
+                //var noarticle = $("#noarticletext").text();
+                //if(noarticle.length > 0) {
+                //    callback(null);
+                //}
 
-            callback(outlinks);
+                var anchors = $("#mw-content-text p").find("a").each(function () {
+                    var url = $(this).attr('href');
+                    if (/^(\/wiki)/.test(url)) {
+                        var splitUrl = url.split("/")[2];
+
+                        if (splitUrl.indexOf("#") > -1)
+                            splitUrl = splitUrl.split("#")[1];
+
+                        if (!/^(Wikipedia|File|Help)/.test(splitUrl))
+                            outlinks.push(splitUrl);
+                    }
+                });
+
+                var categories = [];
+                $("#mw-normal-catlinks ul").find("li").each(function () {
+                    categories.push($(this).find("a").text());
+                });
+
+                var result = {baseurl: link, urls: outlinks, categories: categories};
+
+                callback(result);
+            }
         }
     });
 }
